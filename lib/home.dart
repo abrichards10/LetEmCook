b00099e5dac1fbfa2d57a23eba38bee545905e22
Brooke Richards <54547597+abrichards10@users.noreply.github.com>
2024-06-27 22:54:21 -0700

    add dummy images

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -12,8 +16,41 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(),
+    double width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: AppBar(
+        actions: [Text("Hi")],
+        backgroundColor: Color.fromARGB(255, 162, 227, 174),
+        title: Text(
+          "Let 'Em Cook",
+          style: TextStyle(
+            fontFamily: GoogleFonts.adventPro().fontFamily,
+            fontWeight: FontWeight.bold,
+            fontSize: width * .08,
+          ),
+        ),
+      ),
+      body: Container(
+          child: Column(
+        children: [
+          Center(
+            child: CarouselSlider(
+              items: [
+                Image(image: AssetImage('assets/image1.jpg')),
+                Image(image: AssetImage('assets/image2.jpg')),
+                Image(image: AssetImage('assets/image3.jpg')),
+                Image(image: AssetImage('assets/image4.jpg'))
+              ],
+              options: CarouselOptions(height: 100.0),
+            ),
+          ),
+          Row(
+            children: [Text("Text")],
+          ),
+          Row(children: [Text("Text")])
+        ],
+      )),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:let_em_cook/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Let Em Cook'),
     );
   }
 }
